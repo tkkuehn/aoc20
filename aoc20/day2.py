53f81ccdbc53f71f83a355d4f087de58af3fb6aa
Tristan Kuehn <tristankuehn@gmail.com>
2020-12-02 13:05:48 -0500

    Use cool regex code I found online

--- aoc20/day2.py
+++ aoc20/day2.py
@@ -1,3 +1,5 @@
+import re
+
 import utils
 
 def find_valid_passwords(password_strings):
@@ -19,23 +21,21 @@ def find_valid_passwords_2(password_strings):
     return valid_passwords
 
 def check_password(password_string):
-    policy = password_string.split(": ")[0]
-    password = password_string.split(": ")[1]
-    policy_numbers = policy.split(" ")[0]
-    policy_letter = policy.split(" ")[1]
-    min_letters = int(policy_numbers.split("-")[0])
-    max_letters = int(policy_numbers.split("-")[1])
+    min_letters, max_letters, policy_letter, password = re.match(
+        r"(\d+)-(\d+) ([a-z]): ([a-z]+)",
+        password_string).groups()
+    min_letters = int(min_letters)
+    max_letters = int(max_letters)
     num_letters = password.count(policy_letter)
 
     return min_letters <= num_letters <= max_letters
 
 def check_password_2(password_string):
-    policy = password_string.split(": ")[0]
-    password = password_string.split(": ")[1]
-    policy_numbers = policy.split(" ")[0]
-    policy_letter = policy.split(" ")[1]
-    position_1 = int(policy_numbers.split("-")[0]) - 1
-    position_2 = int(policy_numbers.split("-")[1]) - 1
+    position_1, position_2, policy_letter, password = re.match(
+        r"(\d+)-(\d+) ([a-z]): ([a-z]+)",
+        password_string).groups()
+    position_1 = int(position_1) - 1
+    position_2 = int(position_2) - 1
 
     return ((password[position_1] == policy_letter)
             ^ (password[position_2] == policy_letter))
